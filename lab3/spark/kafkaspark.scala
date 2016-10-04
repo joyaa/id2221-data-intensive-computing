c74ea78a54f3f85dde189368a437181c949b5bc2
joyaa <joya@kth.se>
2016-10-04 22:32:40 +0200

    started lab 3

--- lab3/spark/kafkaspark.scala
+++ lab3/spark/kafkaspark.scala
@@ -1,12 +1,12 @@
 import java.util.HashMap
 
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import kafka.serializer.{DefaultDecoder, StringDecoder}
-
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.storage.StorageLevel
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import kafka.serializer.{DefaultDecoder, StringDecoder}
 
 object KafkaWordCount {
   def main(args: Array[String]) {
@@ -17,24 +17,39 @@ object KafkaWordCount {
 	"group.id" -> "kafka-spark-streaming",
 	"zookeeper.connection.timeout.ms" -> "1000")
 
-    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster(<FILL IN>)
-    val ssc = new StreamingContext(<FILL IN>)
-    ssc.checkpoint("checkpoint")
+  // Create a local StreamingContext with two working thread and batch interval of 2 second
+    //https://spark.apache.org/docs/2.0.0/api/java/org/apache/spark/SparkConf.html
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster("local[2]") //local on 2 cores (threads?)
+    //http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.streaming.StreamingContext
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    ssc.checkpoint("checkpoint") //for stateful transformations and shorten length of dependency chain
 
     // if you want to try the receiver-less approach, comment the below line and uncomment the next one
-    val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](<FILL IN>)
+    //https://spark.apache.org/docs/1.3.0/api/java/index.html?org/apache/spark/streaming/kafka/KafkaUtils.html
+    val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](ssc, kafkaConf) //?????????+
     //val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](<FILL IN>)
 
-    val values = messages.map(<FILL IN>)
-    val pairs = values.<FILL IN>
+    val values = messages.map(_._2) //save the value part of the typle
+    val pairs = values.map((_.split(",")(0), _(1))) //DUBBELKOLLA, https://www.safaribooksonline.com/library/view/learning-spark/9781449359034/ch04.html
 
 
     def mappingFunc(key: String, value: Option[Double], state: State[Double]): Option[(String, Double)] = {
-	<FILL IN>
+/* Förstår ej vad som ska beräknas och vad som finns sparat i state.
+      if (state.exists) {
+
+  	     state = state*
+         val counter = counter + 1
+
+      } else {
+        val initialState = ...
+        state.update(initialState)  // Set the initial state
+      }
+      state.update(average)
+       return (sum / counter)
+*/
     }
 
-
-    val stateDstream = pairs.mapWithState(<FILL IN>)
+    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc)) //Osäkert
 
     stateDstream.print()
     ssc.start()
